6733b5d2b5c947f7dd836fb152385ab603678fc8
SerafimovaD <daci.serafimova@gmail.com>
2023-11-21 13:20:22 +0100

    Add first java program

---
+++ src/proekt_5/Zadaca1.java
@@ -0,0 +1,23 @@
+package proekt_5;
+
+public class Zadaca1 {
+
+	public static void main(String[] args) {
+		        int a = 5; 
+		        int b = 10; 
+		        int c = 3; 
+		        int k;
+		        int d;
+
+		        if (a <= 5 && b > (a - c)) {
+		            k = 1;
+		            System.out.println("k = 1");
+		        } else {
+		            d = 0;
+		            System.out.println("d = 0");
+		        }
+		    }
+
+	}
+
+
